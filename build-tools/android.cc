7e24e084efdf6e7cd6a8a763a5dba9e7bbcf2b8c
Elias Mårtenson <lokedhs@gmail.com>
2014-06-06 16:54:38 +0800

    Added android.cc

--- build-tools/android.cc
+++ build-tools/android.cc
@@ -2,7 +2,19 @@
 
 #include "org_gnu_apl_Native.h"
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+#pragma GCC diagnostic ignored "-Wpragmas"
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#pragma GCC diagnostic ignored "-Wreturn-type"
+#pragma GCC diagnostic ignored "-Wparentheses"
+#pragma GCC diagnostic ignored "-Wreorder"
+#pragma GCC diagnostic ignored "-Wmismatched-tags"
+#pragma GCC diagnostic ignored "-Woverloaded-virtual"
+#include "Native_interface.hh"
 #include "Executable.hh"
+#pragma GCC diagnostic pop
 
 int init_apl( int argc, const char *argv[] );
 
@@ -23,25 +35,6 @@ private:
     jstring jnistring;
     const char *native_string;
 };
-
-/*
-class JniStringReference
-{
-public:
-    JniStringReference( JNIEnv *env_in, const std::string &string_in )
-        : env( env_in )
-        {
-            env->NewStringUTF( string_in.c_str() );
-        }
-
-    ~JniStringReference() { env->DeleteLocalRef( jnistring ); }
-    jstring get_jstring( void ) { return jnistring; }
-
-private:
-    JNIEnv *env;
-    jstring jnistring;
-};
-*/
 
 void throw_apl_exception( JNIEnv *env, const std::string &message )
 {
@@ -80,7 +73,9 @@ JNIEXPORT jobject JNICALL Java_org_gnu_apl_Native_evalExpression( JNIEnv *env, j
         jstring line2 = env->NewStringUTF( UTF8_string( err.get_error_line_2() ).c_str() );
         jstring line3 = env->NewStringUTF( UTF8_string( err.get_error_line_3() ).c_str() );
         jobject ex = env->NewObject( execptionCl, constructor, line1, line2, line3 );
-        env->Throw( (jthrowable)ex );
+        if( ex != NULL ) {
+            env->Throw( (jthrowable)ex );
+        }
         return NULL;
     }
 
