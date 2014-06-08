5cd0843c6069f5def32ef4221b96dfd9c3f82d3e
Elias Martenson <lokedhs@gmail.com>
2014-06-09 02:54:10 +0800

    Updated dir-locals to support OSX

--- build-tools/.dir-locals.el
+++ build-tools/.dir-locals.el
@@ -1,7 +1,10 @@
 ((c++-mode . ((eval . (progn
-                        (em-append-include-dirs (list (expand-file-name "~/src/apl-android/src")
-                                                      (expand-file-name "/usr/local/jdk1.7.0_45/include")
-                                                      (expand-file-name "/usr/local/jdk1.7.0_45/include/linux")))
+                        (em-append-include-dirs (append (list (expand-file-name "~/src/apl-android/src"))
+                                                        (if (eq system-type 'darwin)
+                                                            (list "/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home/include"
+                                                                  "/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home/include/darwin")
+                                                          (list "/usr/local/jdk1.7.0_45/include"
+                                                                "/usr/local/jdk1.7.0_45/include/linux"))))
                         (when (eq system-type 'darwin)
                           (em-append-include-dirs (list "/usr/local/Cellar/gettext/0.18.3.2/include")))
                         (flycheck-mode 1)
