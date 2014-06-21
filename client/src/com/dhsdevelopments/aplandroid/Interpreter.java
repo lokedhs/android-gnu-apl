1bca9c8afbc79885c05bc35aef56cba498cab188
Elias Martenson <lokedhs@gmail.com>
2014-06-22 06:49:11 +0800

    Created an action bar

--- client/src/com/dhsdevelopments/aplandroid/Interpreter.java
+++ client/src/com/dhsdevelopments/aplandroid/Interpreter.java
@@ -2,15 +2,21 @@ package com.dhsdevelopments.aplandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class Interpreter extends Activity
 {
-    /**
-     * Called when the activity is first created.
-     */
     @Override
     public void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.interpreter );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu ) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate( R.menu.main_activity_actions, menu );
+        return super.onCreateOptionsMenu( menu );
     }
 }
